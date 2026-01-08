725287a1847c38dd38a10dab3bd379a5b3a07446
jjmsrc <mnvyiions@gmail.com>
2026-01-08 14:23:34 +0900

    initial commit

---
+++ src/main/java/com/jm/webextfilter/WebExtFilterApplication.java
@@ -0,0 +1,13 @@
+package com.jm.webextfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebExtFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebExtFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jm/webextfilter/WebExtFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.webextfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebExtFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
